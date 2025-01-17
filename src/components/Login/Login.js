b44b16e312f588b9bffbde4cfe15dc219d3d1e05
psychisch <mthuyet294@gmail.com>
2025-01-17 20:45:16 +0700

    create navigation & component login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,10 @@
+const Login = (props) => {
+  return (
+    <div className="login-container">
+      Login
+      <button className="btn btn-primary">Test Bootstrap</button>
+    </div>
+  );
+};
+
+export default Login;
